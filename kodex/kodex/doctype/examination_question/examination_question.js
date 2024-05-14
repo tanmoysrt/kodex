0792d513fd086a9169c86d078d6982b656992bb0
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-05-14 14:26:06 +0530

    feat: doctype update

---
+++ kodex/kodex/doctype/examination_question/examination_question.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, tanmoysrt and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Examination Question", {
+// 	refresh(frm) {
+
+// 	},
+// });
