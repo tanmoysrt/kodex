f8876b68d3d039bd2dc3586cd97833b978e98592
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-05-14 23:46:16 +0530

    feat: strip output and error of code

--- kodex/kodex/doctype/code_runner/code_runner.py
+++ kodex/kodex/doctype/code_runner/code_runner.py
@@ -4,7 +4,6 @@ import frappe
 from frappe.model.document import Document
 from kodex.utils import judge0
 import base64
-import time
 
 class CodeRunner(Document):
 	def before_insert(self):
@@ -24,17 +23,17 @@ def check_code_submission_results():
 			doc = frappe.get_doc("Code Runner", code.name)
 			if result["status"]["id"] == 3:
 				doc.status = "completed"
-				doc.output = base64.b64decode(result["stdout"]).decode("utf-8") if result["stdout"] else ""
-				doc.error = base64.b64decode(result["stderr"]).decode("utf-8") if result["stderr"] else ""
-				doc.compile_output = base64.b64decode(result["compile_output"]).decode("utf-8") if result["compile_output"] else ""
+				doc.output = (base64.b64decode(result["stdout"]).decode("utf-8") if result["stdout"] else "").rstrip()
+				doc.error = (base64.b64decode(result["stderr"]).decode("utf-8") if result["stderr"] else "").rstrip()
+				doc.compile_output = (base64.b64decode(result["compile_output"]).decode("utf-8") if result["compile_output"] else "").rstrip()
 				doc.time_second = float(result["time"])
 				doc.memory_kb = result["memory"]
 				doc.debug_judge0_status_id = result["status"]["id"]
 				doc.debug_judge0_status_description = result["status"]["description"]
 			else:
 				doc.status = "failed"
-				doc.error = base64.b64decode(result["stderr"]).decode("utf-8") if result["stderr"] else ""
-				doc.compile_output = base64.b64decode(result["compile_output"]).decode("utf-8") if result["compile_output"] else ""
-				doc.debug_judge0_status_id = result["status"]["id"]
-				doc.debug_judge0_status_description = result["status"]["description"]
+				doc.error = (base64.b64decode(result["stderr"]).decode("utf-8") if result["stderr"] else "").rstrip()
+				doc.compile_output = (base64.b64decode(result["compile_output"]).decode("utf-8") if result["compile_output"] else "").rstrip()
+				doc.debug_judge0_status_id = (result["status"]["id"]).rstrip()
+				doc.debug_judge0_status_description = (result["status"]["description"]).rstrip()
 			doc.save()
